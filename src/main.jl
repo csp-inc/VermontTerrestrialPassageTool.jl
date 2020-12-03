f6aa5ccc41b176f0750b3b1169633334a4d9f927
Vincent Landau <vincent@csp-inc.org>
2020-12-02 17:19:32 -0700

    track missing structures and skip over failed structures

--- src/main.jl
+++ src/main.jl
@@ -26,18 +26,6 @@ function compute_all_culverts(path::String)
     input_resolution = 0.5
     side_length_meters = (os_radius_pixels + summary_radius_pixels) * input_resolution * 2
 
-    ## Check that all ids in culvert_ids are in the shapefile
-    culvert_shapefile_ids = string.(getproperty(Shapefile.Table(culvert_shapefile_path), culvert_id_field))
-    bad_ids = culvert_ids[(!).(map(x -> in(x, culvert_shapefile_ids), culvert_ids))]
-    if !isempty(bad_ids)
-        @warn("The following user-provided culvert IDs are missing from the culverts shapefile and will be skipped.")
-        print("    ")
-        for c in 1:length(bad_ids)
-            print(string(bad_ids[c], "  "))
-        end
-        culvert_ids = culvert_ids[map(x -> in(x, culvert_shapefile_ids), culvert_ids)]
-    end
-
     ## Setup output file structure and write empty file to store metadata
     # Create directory
     dir_suffix = 2
@@ -52,6 +40,22 @@ function compute_all_culverts(path::String)
         output_folder = string(output_folder, "_$(dir_suffix)")
     end
     mkpath(output_folder)
+
+    ## Check that all ids in culvert_ids are in the shapefile
+    culvert_shapefile_ids = string.(getproperty(Shapefile.Table(culvert_shapefile_path), culvert_id_field))
+    bad_ids = culvert_ids[(!).(map(x -> in(x, culvert_shapefile_ids), culvert_ids))]
+    if !isempty(bad_ids)
+        @warn("The following user-provided culvert IDs are missing from the culverts shapefile and will be skipped. $(bad_ids)")
+
+        missing_str_file = string(output_folder, "/missing_structures.txt")
+        touch(missing_str_file)
+
+        open(missing_str_file, "a") do f
+          writedlm(f, bad_ids, ",")
+        end
+
+        culvert_ids = culvert_ids[map(x -> in(x, culvert_shapefile_ids), culvert_ids)]
+    end
 
     # Create metadata CSV
     results_file = string(output_folder, "/results_summary.csv")
@@ -84,44 +88,60 @@ function compute_all_culverts(path::String)
     total_solves = length(culvert_ids)
     for i in 1:total_solves
         @info "Solving culvert $(culvert_ids[i]), $(i) of $(total_solves)"
-        os_args["project_name"] = string(output_folder, "/", culvert_ids[i])
-        local output = compute_connectivity(culvert_shapefile_path,
-                                      culvert_id_field,
-                                      String(culvert_ids[i]),
-                                      landcover_path,
-                                      side_length_meters,
-                                      source_strength_path,
-                                      mask_values,
-                                      os_args,
-                                      reclass_table)
+        try
+            os_args["project_name"] = string(output_folder, "/", culvert_ids[i])
+            local output = compute_connectivity(culvert_shapefile_path,
+                                                culvert_id_field,
+                                                String(culvert_ids[i]),
+                                                landcover_path,
+                                                side_length_meters,
+                                                source_strength_path,
+                                                mask_values,
+                                                os_args,
+                                                reclass_table)
 
-        cum_current, geotransform, x_coord, y_coord = output
-        # Clip current map for summary, should always be an odd number of rows and
-        # columns based on internal code
-        culvert_row_y = Int(ceil((geotransform[4] - y_coord) / input_resolution)) + 1
-        culvert_col_x = Int(floor((x_coord - geotransform[1]) / input_resolution)) + 1
-        current_clipped = Omniscape.clip(cum_current, x = culvert_col_x,
-                                         y = culvert_row_y,
-                                         distance = summary_radius_pixels)
+            cum_current, geotransform, x_coord, y_coord = output
+            # Clip current map for summary, should always be an odd number of rows and
+            # columns based on internal code
+            culvert_row_y = Int(ceil((geotransform[4] - y_coord) / input_resolution)) + 1
+            culvert_col_x = Int(floor((x_coord - geotransform[1]) / input_resolution)) + 1
+            current_clipped = Omniscape.clip(cum_current, x = culvert_col_x,
+                                             y = culvert_row_y,
+                                             distance = summary_radius_pixels)
 
-        current_mean = mean(skipmissing(current_clipped))
-        current_sd = sqrt(var(skipmissing(current_clipped)))
-        current_min = minimum(skipmissing(current_clipped))
-        current_max = maximum(skipmissing(current_clipped))
+            current_mean = mean(skipmissing(current_clipped))
+            current_sd = sqrt(var(skipmissing(current_clipped)))
+            current_min = minimum(skipmissing(current_clipped))
+            current_max = maximum(skipmissing(current_clipped))
 
 
-        row_values = hcat([String(culvert_ids[i])],
-                          [x_coord],
-                          [y_coord],
-                          [current_mean],
-                          [current_sd],
-                          [current_min],
-                          [current_max])
+            row_values = hcat([String(culvert_ids[i])],
+                              [x_coord],
+                              [y_coord],
+                              [current_mean],
+                              [current_sd],
+                              [current_min],
+                              [current_max])
 
-        open(results_file, "a") do io
-          writedlm(io, row_values, ",")
+            open(results_file, "a") do io
+              writedlm(io, row_values, ",")
+            end
+        catch e
+            @warn "Solve failed for culvert with ID $(culvert_ids[i]). Skipping."
+
+            # Keep track of failed structures and their error messages
+            if !isfile(string(output_folder, "/failed_structures.csv"))
+                touch(string(output_folder, "/failed_structures.csv"))
+                open(string(output_folder, "/failed_structures.csv"), "a") do f
+                  writedlm(f, ["Culvert ID" "Error Message"], ",")
+                end
+            end
+
+            error_message = sprint(showerror, e, stacktrace())
+            open(string(output_folder, "/failed_structures.csv"), "a") do f
+              writedlm(f, [culvert_ids[i] String(error_message)], ",")
+            end
         end
-
         println(" ")
     end
 
